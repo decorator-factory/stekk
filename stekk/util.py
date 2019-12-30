b1127a1e9e91bbb49400ed85f41e29ec7ee0022c
decorator-factory <municipalomnibenevolence@yandex.ru>
2019-12-30 13:42:29 +0300

    create util submodule

---
+++ stekk/util.py
@@ -0,0 +1,19 @@
+# from Stackoverflow
+
+import functools
+class StrWrapper(object):
+    def __init__(self, repr_, func):
+        self._repr = repr_
+        self._func = func
+        functools.update_wrapper(self, func)
+
+    def __call__(self, *args, **kwargs):
+        return self._func(*args, **kwargs)
+
+    def __str__(self):
+        return self._repr(self._func)
+
+def withrepr(strfun):
+    def _wrap(func):
+        return StrWrapper(strfun, func)
+    return _wrap
