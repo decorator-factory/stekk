a124b4923f9a029ac745fc0459a48c309f443f04
decorator-factory <municipalomnibenevolence@yandex.ru>
2019-12-31 13:44:42 +0300

    allow running multiple files in sequence

--- stekk/__main__.py
+++ stekk/__main__.py
@@ -1,17 +1,23 @@
-from . import console, loadf
-from .parser import StekkSyntaxError
+from . import loadf
+from .interactive import console
+from .parser import StekkSyntaxError, parse
+from .vm import VM
 import sys
 
 if len(sys.argv) == 1:
     console()
-elif len(sys.argv) == 2:
-    filename = sys.argv[1]
-    try:
-        vm = loadf(filename)
-    except FileNotFoundError:
-        print("File not found:", filename)
-    except StekkSyntaxError as e:
-        print(e.error)
-    else:
-        vm.run()
-        console(vm)
+elif len(sys.argv) > 1:
+    filenames = sys.argv[1:]
+    vm = VM([])
+    for filename in filenames:
+        try:
+            statements = loadf(filename).statements
+            vm.statements.extend(statements)
+        except FileNotFoundError:
+            print("File not found:", filename)
+            exit(1)
+        except StekkSyntaxError as e:
+            print(e.error)
+            exit(2)
+    vm.run()
+    console(vm)
